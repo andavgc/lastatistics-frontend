6d1a19bbac5811f15313a7293d888007bfcccd55
andav <andav_@gitlab.com>
2023-04-05 18:22:58 -0400

    changing title

--- src/paginas/datos/index.js
+++ src/paginas/datos/index.js
@@ -6,7 +6,7 @@ import Card from '../../components/card';
 
 export default function ApiData() {
 
-  const { user, period, limit, periodValue } = useContext(UserContext);
+  const { user, period, limit, periodValue, method } = useContext(UserContext);
   const [tracks, setTracks] = useState([]);
 
 
@@ -24,7 +24,7 @@ export default function ApiData() {
 
   return (
     <>
-      <h1 className={styles.titulo}>{user}'s top {limit} tracks {periodValue}</h1>
+      <h1 className={styles.titulo}>{user}'s top {limit} {method.substr(3, -1)} {periodValue}</h1>
       <div className={styles.container}>
       {tracks?.map(track => <Card key={track.name} data={track}/>)}
       </div>
