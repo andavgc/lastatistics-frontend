b1721f338d619ce94202065f60e4b1855bb65e67
andav <andav_@gitlab.com>
2023-04-07 13:49:48 -0400

    handling with data errors

--- src/paginas/datos/index.js
+++ src/paginas/datos/index.js
@@ -13,21 +13,24 @@ export default function ApiData() {
   useEffect(() => {
 
     axios.get(`https://lastatistics-api.vercel.app/api/music-info/${user}`)
-        .then((resp) => {
+      .then((resp) => {
         setTracks(resp.data.infolist);
       })
       .catch((error) => console.log(error))
-    
+
 
   }, [user, period, limit])
 
 
   return (
-    <>
-      <h1 className={styles.titulo}>{user}'s top {limit} {methodValue.substr(3)} ({periodValue})</h1>
-      <div className={styles.container}>
-      {tracks?.map(track => <Card key={track.name} data={track}/>)}
-      </div>
-    </>
+      <>
+      <>
+        {user? <h1 className={styles.titulo}>{user}'s top {limit} {methodValue.substr(3)} ({periodValue})</h1>:<></>}
+      </>
+        
+        <div className={styles.container}>
+          {tracks!==undefined && tracks.length!==0? <> {tracks.map(track => <Card key={track.name} data={track} />)}</>:<h2 className={styles.error__message}>Oops... looks like there's no data to show here :(</h2>}
+        </div>
+      </>
   )
 }
